7d9d91beab49720d5372e45a4526a60f1d1dd54d
rezina-git <budhathokirezina59@gmail.com>
2025-04-28 20:03:22 +0545

    Learned about Conditional Statements

--- Day05_fromYT.py
+++ Day05_fromYT.py
@@ -23,3 +23,59 @@ print("Average is :",(f1 + f2)/2)
 a = int(input("Enter fist num:"))
 b = int (input("Enter the second num:"))
 print(a>=b)
+
+#Control Statements
+light = "green"
+
+if(light=="red"):
+    print("Stop")
+elif(light == "green"):
+    print("GO")
+elif(light == "Yellow"):
+    print("Look")
+else:
+    print("Light is broken")
+#else is always written in last that means if all above above condition is wrong then else statement will run.
+
+#Grade students based on marks:
+#marks>=90, grade = "A"
+#90>marks>=80, grade = "B"
+#80>marks>=70,grade = "C"
+#70>marks,grade ="D"
+
+marks = int(input("Enter the marks:"))
+if (marks>=90):
+    print("Grade A")
+elif(marks>90 and marks>=80):
+    print("Grade B")
+elif(marks>80 and marks>=70):
+    print("Grade C")
+else:
+    print("Grade D")
+    
+#EX 5: 
+#WAP to check if a number entered by the user is ODD or EVEN
+num = int(input("Enter a number:"))
+if(num%2==0):
+    print("NUmber is EVEN")
+else:
+    print("ODD")
+    
+#WAP to find the greatest of three numbers entered by the user.
+a = int(input("Enter a first num:"))
+b = int(input("Enter a second num:"))
+c = int(input("Enter third num:"))
+if(a>b and a>c):
+    print("A is greatest",a)
+elif(b>c and b>a):
+    print("B is greatest",b)
+else:
+    print("C is greatest",c)
+
+#WAP to check if a number is multiple of 7 or not.
+
+D = int(input("Enter a num to check is it multiple of 7:"))
+if (D%7 == 0):
+    print("Number is multiple of  7")
+else:
+    print("NOt a multiple of 7")
